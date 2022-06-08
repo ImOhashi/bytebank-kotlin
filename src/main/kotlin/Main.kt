bf92871c64581a17156579a28c7fcf016fd062f3
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-06-08 01:45:47 -0300

    while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    for (i in 1..5) {
+    var i = 0
 
+    while (i < 5) {
         val titular: String = "Leonardo $i"
         val numeroConta: Int = 1000 + i
         var saldo: Double = i + 100.0
@@ -12,7 +13,22 @@ fun main() {
                     "Numero da conta: $numeroConta\n" +
                     "Saldo da conta: $saldo"
         )
+
+        i++
     }
+
+//    for (i in 1..5) {
+//
+//        val titular: String = "Leonardo $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo: Double = i + 100.0
+//
+//        println(
+//            "\nTitular: $titular\n" +
+//                    "Numero da conta: $numeroConta\n" +
+//                    "Saldo da conta: $saldo"
+//        )
+//    }
 
     //testCondition(saldo)
 }
