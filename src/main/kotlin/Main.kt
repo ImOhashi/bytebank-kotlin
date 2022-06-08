f227654056cae700160e6ee75c3acf3c0be718dc
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-06-08 00:47:28 -0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    println("Bem vindo ao Bytebank")
+
+    val titular: String = "Leonardo"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+
+    saldo = 100.0
+
+    println("\nTitular: $titular\n" +
+        "Numero da conta: $numeroConta\n" +
+            "Saldo da conta: $saldo")
+
+//    if (saldo > 0.0) {
+//        println("Conta é positiva!")
+//    } else if(saldo == 0.0) {
+//        println("Conta está zerada!")
+//    } else {
+//        println("Conta está negativa!")
+//    }
+
+    when {
+        saldo > 0.0 -> {
+            println("Conta está positiva!")
+        }
+        saldo == 0.0 -> {
+            println("Conta está zerada!")
+        }
+        else -> {
+            println("Conta está negativa!")
+        }
+    }
+}
