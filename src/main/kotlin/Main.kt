ba40c33edbc2296a2e53433fc0a140001d2a501e
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-06-21 00:23:42 -0300

    fix: test account

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,5 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    val accountLeo = Account(holder = "Leo", number = 1001)
-    accountLeo.deposit(200.0)
-
-    val accountAlex = Account(number = 1002, holder = "Alex")
-    accountAlex.deposit(200.0)
-
-    println(accountLeo.holder)
-    println(accountLeo.number)
-    println(accountLeo.balance)
-
-    println(accountAlex.holder)
-    println(accountAlex.number)
-    println(accountAlex.balance)
-
-    println("Depositando na conta do Alex")
-    accountAlex.deposit(50.0)
-    println(accountAlex.balance)
-
-    accountAlex.withdrawMoney(15.0)
-    println(accountAlex.balance)
-
-    accountLeo.transfer(100.0, accountAlex)
-
-    accountAlex.withdrawMoney(500.0)
-
-    println(accountLeo.balance)
-    println(accountAlex.balance)
+    testAccount()
 }
